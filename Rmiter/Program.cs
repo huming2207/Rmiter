02c431b31e097fd941bd5fb7525dd69f0763682d
Jackson Ming Hu <huming2207@gmail.com>
2017-02-28 12:25:06 +1100

    Add test

--- Rmiter/Program.cs
+++ Rmiter/Program.cs
@@ -4,11 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Diagnostics;
 using System.Security;
-using RmiterCore;
-using RmiterCore.MyRmit;
-using RmiterCorePcl.MyRmit;
-using RmiterCore.LibraryInfo;
 
 namespace RmiterDemo
 {
@@ -16,7 +13,8 @@ namespace RmiterDemo
     {
         static void Main(string[] args)
         {
-            RmiterCorePcl.CasLogin casLogin = new RmiterCorePcl.CasLogin();
+            var casLoginPcl = new RmiterCorePcl.CasLogin();
+            var casLogin = new RmiterCore.CasLogin();
 
             Console.WriteLine("[Demo #1] Get myRMIT announcement messages' title");
 
@@ -52,8 +50,22 @@ namespace RmiterDemo
 
 
             Console.WriteLine("\n[Info] Please wait...");
+            Console.WriteLine("[Info] Preparing stopwatch...");
+            var stopWatch = new Stopwatch();
 
-            CookieContainer cookie = casLogin.RunCasLogin(username, password).Result;
+            // Test login with PCL library
+            stopWatch.Start();
+            var cookiePcl = casLoginPcl.RunCasLogin(username, password).Result;
+            stopWatch.Stop();
+            string casLoginPclTime = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
+
+            // Test login with classic library
+            stopWatch.Start();
+            var cookie = casLogin.RunCasLogin(username, password).Result;
+            stopWatch.Stop();
+            string casLoginTime = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
 
             // Clear up the password variable to ensure it's not being (easily) captured later
             password = "";
@@ -70,10 +82,24 @@ namespace RmiterDemo
             }
 
             Console.WriteLine("[Info] Grabbing announcement messages' title...\n\n");
-            RmiterCorePcl.MyRmit.MyRmitPortal portal = new RmiterCorePcl.MyRmit.MyRmitPortal(cookie);
-            var homeObject = portal.GetHomeMessages().Result;
 
-            foreach(var announcement in homeObject.Announcements)
+            // Test on accesing myRMIT portal via portable library
+            stopWatch.Start();
+            var portalPcl = new RmiterCorePcl.MyRmit.MyRmitPortal(cookiePcl);
+            var homeObjectPcl = portalPcl.GetHomeMessages().Result;
+            stopWatch.Stop();
+            string myRmitTimePcl = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
+
+            // Test on access myRMIT portal via classic library
+            stopWatch.Start();
+            var portal = new RmiterCore.MyRmit.MyRmitPortal(cookie);
+            var homeObject = portal.GetHomeMessages().Result;
+            stopWatch.Stop();
+            string myRmitTime = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
+
+            foreach (var announcement in homeObject.Announcements)
             {
                 Console.WriteLine("[Title] \"{0}\"", announcement.Title);
             }
@@ -81,13 +107,28 @@ namespace RmiterDemo
             Console.WriteLine("\n[Info] Demo #1 finished, continue on the next one...");
             Console.WriteLine("[Demo #2] Get Carlton library's opening hour, DOES NOT need to login this time.");
 
-            var libraryInfoParser = new InfoParser();
-            var libraryInfoResult = libraryInfoParser.GetOpeningHours().Result;
+            // Test on RmiterCorePcl.LibraryInfo.InfoParser
+            stopWatch.Start();
+            var libraryInfoParserPcl = new RmiterCorePcl.LibraryInfo.InfoParser();
+            var libraryInfoResultPcl = libraryInfoParserPcl.GetOpeningHours().Result;
+            stopWatch.Stop();
+            string libInfoPclTime = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
 
-            Console.Write("[Info] Today's Carlton library opening hour is: ");
+            // Test on RmiterCorePcl.LibraryInfo.InfoParser
+            stopWatch.Start();
+            var libraryInfoParser = new RmiterCore.LibraryInfo.InfoParser();
+            var libraryInfoResult = libraryInfoParserPcl.GetOpeningHours().Result;
+            stopWatch.Stop();
+            string libInfoTime = stopWatch.ElapsedMilliseconds.ToString();
+            stopWatch.Reset();
 
-            Console.WriteLine(libraryInfoResult.CarltonLibrary);
+            Console.WriteLine("[Info] Today's Carlton library opening hour is (from PCL): {0}", libraryInfoResultPcl.CarltonLibrary);
 
+            Console.WriteLine("\n\n[Performance] Performance details are shown below: ");
+            Console.WriteLine("[Performance] CAS Login, Classic (via HAP Engine): {0} ms; PCL (via Jumony Engine): {1} ms", casLoginTime, casLoginPclTime);
+            Console.WriteLine("[Performance] myRMIT Portal, Classic (JSON.NET): {0} ms; PCL (JSON.NET PCL): {1} ms", myRmitTime, myRmitTimePcl);
+            Console.WriteLine("[Performance] Library opening hours, Classic (via HAP Engine): {0} ms; PCL (via Jumony Engine): {1} ms", libInfoTime, libInfoPclTime);
             Console.WriteLine("\n\n[Info] Demo finished. Press any key to exit.");
             Console.ReadKey();
         }
---
+++ RmiterCorePcl/LibraryInfo/InfoParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Ivony.Html;
+using Ivony.Html.Parser;
+
+namespace RmiterCorePcl.LibraryInfo
+{
+    public class InfoParser
+    {
+        private HttpClient _LibraryHttpClient(string baseUrl = "http://www.lib.rmit.edu.au")
+        {
+            // HttpClient declaration
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            client.DefaultRequestHeaders.Add("User-Agent", UserAgentGenerator.GenerateUserAgent());
+            return client;
+        }
+
+        public async Task<OpeningHours> GetOpeningHours()
+        {
+            // Declare HttpClient
+            var httpClient = _LibraryHttpClient();
+            string infoRawHtml = await httpClient.GetStringAsync("/hours/today.php");
+
+            // Workaround: when the library is closed, the </td> tag is missing,
+            //   which causes the HAP engine cannot parse the HTML properly.
+            // (i.e. they wrote "<td>CLOSED" instead of the "correct" one which is "<td>CLOSED</td>")
+            if (infoRawHtml.Contains("<td>CLOSED") && !infoRawHtml.Contains("<td>CLOSED</td>"))
+            {
+                infoRawHtml.Replace("<td>CLOSED", "<td>CLOSED</td>");
+            }
+
+            // Declare and prepare for the Jumony engine
+            var jumonyParser = new JumonyParser();
+            var htmlDoc = jumonyParser.Parse(infoRawHtml);
+
+            // Grab all contents with "<td>" tags
+            var tableContents = htmlDoc.Find("td");
+            var pickedList = new List<string>();
+            
+            for(int index = 1; index <= 10; index++)
+            {
+                string tableContent = tableContents.ElementAt(index - 1).InnerText();
+
+                // Have a look at the structure of the table and you'll know why.
+                // Only the even-number-index content is the REAL time string.
+                if(index % 2 == 0)
+                {
+                    pickedList.Add(tableContent);
+                }
+            }
+
+            var infoResult = new OpeningHours()
+            {
+                BrunswickLibrary = pickedList[0],
+                BundooraLibrary = pickedList[1],
+                BundooraEastLibrary = pickedList[2],
+                CarltonLibrary = pickedList[3],
+                SwanstonLibrary = pickedList[4]
+            };
+
+            return infoResult;
+        }
+    }
+}

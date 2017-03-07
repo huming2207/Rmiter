b91a6652364696da1a77c8cda569055ff1e349aa
Jackson Ming Hu <huming2207@gmail.com>
2017-03-07 13:33:26 +1100

    Make the backend core library GREAT AGAIN!
    Added` a tricky workaround, microsoft made a weird bug on System.Net.Http
    library. The library will become an idiot and refuse to handle the
    cookies well when 302 Redirection occurs. Meanwhile, it seems that
    Microsoft forgot (or ignored? rejected?) to fix this as the bug has been
    found long ago.
    (see: https://github.com/dotnet/corefx/issues/9003)

--- RmiterCoreUwp/MyRmit/MyRmitPortal.cs
+++ RmiterCoreUwp/MyRmit/MyRmitPortal.cs
@@ -22,23 +22,26 @@ namespace RmiterCoreUwp.MyRmit
         {
             var clientHandler = new HttpClientHandler()
             {
-                CookieContainer = cookieContainer
+                CookieContainer = cookieContainer,
+                AllowAutoRedirect = false
             };
 
             // HttpClient declaration
             var client = new HttpClient(clientHandler)
             {
-                BaseAddress = new Uri(baseUrl),
+                BaseAddress = new Uri(baseUrl)
             };
 
             return client;
         }
 
+        
+
         private async Task<T> _GetDataAsync<T>(string queryPath)
         {
-            var client = _MyRmitHttpClient();
-            string jsonStr = await client.GetStringAsync(queryPath);
-
+            var httpResponse = await GetWithManualRedirectionAsync(queryPath);
+            
+            string jsonStr = await httpResponse.Content.ReadAsStringAsync();
             var jsonSettings = new JsonSerializerSettings()
             {
                 NullValueHandling = NullValueHandling.Ignore
@@ -47,6 +50,43 @@ namespace RmiterCoreUwp.MyRmit
             var jsonObject = JsonConvert.DeserializeObject<T>(jsonStr, jsonSettings);
             return jsonObject;
         }
+
+        private async Task<HttpResponseMessage> GetWithManualRedirectionAsync(string queryPath, string baseUrl = "https://my.rmit.edu.au")
+        {
+            var clientHandler = new HttpClientHandler()
+            {
+                CookieContainer = cookieContainer,
+
+                // Disable the original shitty auto redirect function who won't treat cookies well.
+                AllowAutoRedirect = false
+            };
+
+            var httpClient = new HttpClient(clientHandler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            var httpResponse = await httpClient.GetAsync(queryPath);
+
+            // If it's not HTTP 302 response, return the HttpResponseMessage
+            // If it is, then do a manual redirection (follow the content in HTTP headers)
+            if (httpResponse.StatusCode != HttpStatusCode.Redirect)
+            {
+                return httpResponse;
+            }
+            else
+            {
+                // Get the redirect URL from redirection headers, get the first one (and the only one)
+                //  then remove the base URL, prepare to run the GET again.
+                string redirectQueryPath = httpResponse.Headers.GetValues("Location")
+                    .First()
+                    .Replace(baseUrl, "");
+
+                return await GetWithManualRedirectionAsync(redirectQueryPath);
+            }
+
+        }
+
 
         public async Task<Home> GetHomeMessages()
         {
